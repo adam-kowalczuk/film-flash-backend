116ab27c121bd058ba7347c2a2d14f89ba5d4916
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-07-13 23:18:39 -0700

    Create 'videos' serverless function

---
+++ netlify/functions/videos/videos.js
@@ -0,0 +1,34 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+import fetch from "node-fetch";
+
+export const handler = async (event) => {
+  const movieId = JSON.parse(event.body);
+
+  const API_KEY = process.env.API_KEY;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`
+    }
+  };
+
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+  };
+
+  const TMDB_API = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&append_to_response=videos`;
+
+  const response = await fetch(TMDB_API, options);
+  console.log("Response: ", response);
+  data = await response.json();
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(data)
+  };
+};
