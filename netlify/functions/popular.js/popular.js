9bbadda47a1b0afc670884605aeef1a6f2ac9c29
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-07-13 21:18:06 -0700

    Add 'popular' serverless function

---
+++ netlify/functions/popular.js/popular.js
@@ -0,0 +1,32 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+
+const fetch = require("node-fetch");
+
+const handler = async () => {
+  const API_KEY = process.env.API_KEY;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: API_KEY
+    }
+  };
+
+  const TMDB_API =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+  const response = await fetch(TMDB_API, options);
+  console.log(response);
+  data = await response.json();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ data })
+  };
+};
+
+module.exports = { handler };
