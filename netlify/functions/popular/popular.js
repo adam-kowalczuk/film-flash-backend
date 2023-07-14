349bf52af63149f24ac40005294b6543f024ad23
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-07-13 22:08:07 -0700

    Reapply fetch

--- netlify/functions/popular/popular.js
+++ netlify/functions/popular/popular.js
@@ -1,17 +1,14 @@
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 const fetch = require("node-fetch");
 
-export const handler = async () => {
+const handler = async () => {
   const API_KEY = process.env.API_KEY;
 
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: API_KEY,
-      "Access-Control-Allow-Origin": "*", // Set the appropriate origin here
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization" // Add any additional headers required
+      Authorization: API_KEY
     }
   };
 
@@ -30,3 +27,5 @@ export const handler = async () => {
     body: JSON.stringify({ data })
   };
 };
+
+modules.export = { handler };
